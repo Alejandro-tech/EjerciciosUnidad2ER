b88e334ba5b0656c7c6fd65e5b2bcb0fc9281912
Alejandro-tech <53318018+Alejandro-tech@users.noreply.github.com>
2020-11-03 09:53:23 -0600

    Add files via upload

--- EjerciciosU2_BalamPechJulianAlejandro.py
+++ EjerciciosU2_BalamPechJulianAlejandro.py
@@ -3,22 +3,9 @@ import re
 txt = """ El Universo es el espacio y el tiempo que abarca todo aquello que existe, es decir, todos los tipos de materias, los planetas, la energía, la luz, las estrellas, los satélites, las galaxias y otros objetos celestes, incluso, las leyes y las constantes físicas que los gobiernan. Por ello, el Universo es difícil de explicar o medir.
 El conocimiento científico actual ha determinado que el tamaño del Universo es muy grande Mdiendo, lo que dificulta su cálculo a las 12:23 am, ya que no se sabe con certeza cuáles son sus límites, y esa misma grandeza hace que sea considerado como infinito.
 Asimismo, algunos investigadores científicos defienden que existen varias dimensiones que “forman universos coexistentes e interpenetrantes”, que no se mezclan. En diferentes códigos postales 77300, 71200, 19192, 88122, 09221, 82122, 88221. 
-Edad de Piedra. Msxico Primer periodo de la Prehistoria durante el cual, los seres humanos fabricaron herramientas de piedra, siendo la tecnología más avanzada en ese entonces. La madera, los huesos y otros materiales también fueron utilizados (cuernos, cestos, cuerdas, etc), pero la piedra (en particular, varias rocas de ruptura concoidea, es decir, con puntas cortantes, como la silex, el cuarzo, cuarcita, obsidiana, etc) fue utilizada para fabricar herramientas y armas, de corte o percusión. La Prehistoria se divide en Edad de Piedra y Edad de los Metales, luego de esos periodos se inicia la Historia propiamente dicha, es decir, se utiliza la escritura para describir los hechos al número 982 172 80 93, 123 129 82 65, 203 120 02 12, 984 185 22 45, 283 192 66 43.
+Edad de Piedra. Msxico Primer periodo de la Prehistoria durante el cual, los seres humanos fabricaron herramientas de piedra, siendo la tecnología más avanzada en ese entonces. La madera, los huesos y otros materiales también fueron utilizados (cuernos, cestos, cuerdas, etc), pero la piedra (en particular, varias rocas de ruptura concoidea, es decir, con puntas cortantes, como la silex, el cuarzo, cuarcita, obsidiana, etc) fue utilizada para fabricar herramientas y armas, de corte o percusión. La Prehistoria se divide en Edad de Piedra y Edad de los Metales 77405, luego de esos periodos se inicia la Historia propiamente dicha, es decir, se utiliza la escritura para describir los hechos al número 982 172 80 93, 123 129 82 65, 203 120 02 12, 984 185 22 45, 283 192 66 43.
 Sin embargo, esta es la condición necesaria a las 10:23 pm, pero no suficiente para definir este periodo "Edad de Piedra", ya que en el tuvieron lugar fenómenos fundamentales para el ser humano, en cuanto adquisiciones tecnológicas (fuego, instrumentos, vivienda, ropa, etc), la evolución social, los cambios climáticos, la revolución económica desde un sistema cazador-recolector a las 07:56 am, hasta un sistema parcialmente productor, la diáspora del ser humano por todo el mundo habitable, desde la "Cuna de la humanidad" el continente Africano, entre otros acontecimientos al correo alejandro@gmail.com , sony@gmail.com , buenavista@gmail.com , vilca@gmail.com , alone@gmail.com .
 Lo primero que debes saber es qué significa IP: son las iniciales de Internet Protocol, que traducido al español lo podemos llamar como Protocolo de Internet a las 09:23 pm. En otras palabras, es el sistema estándar mediante el cual funciona la internet, por medio de un proceso de envío y recepción de información. Unos ejemplos son 192.168.12.15, 192.168.00.12, 192.168.122.242, 192.168.23.22. En diferentes urls como https://whatsapp.com , https://facebook.com , https://amazon.com , https://mercadolibre.com """
-
-print("Elija la opción que desee ejecutar")
-print("1. Todas las palabras que tengan una longitud de 7 o más letras\n" 
-"2.	Expresiones que NO finalicen con una vocal\n"
-"3.	Las palabras que inicien con “M” donde la segunda letra no sea voca\n"
-"4.	Expresiones encerradas entre comillas\n"
-"5.	Ip’s\n"
-"6.	Horas\n"
-"7.	Telefonos\n"
-"8.	Correos electrónicos\n"
-"9.	Url´s\n"
-"10. Código postal\n")
-opcion = int( input ("Ingresa la opción que desea ejecutar"))
 
 print("-----------------------------------------------------------------------")
 print("1.	Todas las palabras que tengan una longitud de 7 o más letras")
