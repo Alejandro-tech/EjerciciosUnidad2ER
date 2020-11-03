98424162fce96081bce216e792bf569bc8d3c282
Alejandro-tech <53318018+Alejandro-tech@users.noreply.github.com>
2020-11-03 09:46:51 -0600

    Add files via upload

--- EjerciciosU2_BalamPechJulianAlejandro.py
+++ EjerciciosU2_BalamPechJulianAlejandro.py
@@ -7,6 +7,19 @@ Edad de Piedra. Msxico Primer periodo de la Prehistoria durante el cual, los ser
 Sin embargo, esta es la condición necesaria a las 10:23 pm, pero no suficiente para definir este periodo "Edad de Piedra", ya que en el tuvieron lugar fenómenos fundamentales para el ser humano, en cuanto adquisiciones tecnológicas (fuego, instrumentos, vivienda, ropa, etc), la evolución social, los cambios climáticos, la revolución económica desde un sistema cazador-recolector a las 07:56 am, hasta un sistema parcialmente productor, la diáspora del ser humano por todo el mundo habitable, desde la "Cuna de la humanidad" el continente Africano, entre otros acontecimientos al correo alejandro@gmail.com , sony@gmail.com , buenavista@gmail.com , vilca@gmail.com , alone@gmail.com .
 Lo primero que debes saber es qué significa IP: son las iniciales de Internet Protocol, que traducido al español lo podemos llamar como Protocolo de Internet a las 09:23 pm. En otras palabras, es el sistema estándar mediante el cual funciona la internet, por medio de un proceso de envío y recepción de información. Unos ejemplos son 192.168.12.15, 192.168.00.12, 192.168.122.242, 192.168.23.22. En diferentes urls como https://whatsapp.com , https://facebook.com , https://amazon.com , https://mercadolibre.com """
 
+print("Elija la opción que desee ejecutar")
+print("1. Todas las palabras que tengan una longitud de 7 o más letras\n" 
+"2.	Expresiones que NO finalicen con una vocal\n"
+"3.	Las palabras que inicien con “M” donde la segunda letra no sea voca\n"
+"4.	Expresiones encerradas entre comillas\n"
+"5.	Ip’s\n"
+"6.	Horas\n"
+"7.	Telefonos\n"
+"8.	Correos electrónicos\n"
+"9.	Url´s\n"
+"10. Código postal\n")
+opcion = int( input ("Ingresa la opción que desea ejecutar"))
+
 print("-----------------------------------------------------------------------")
 print("1.	Todas las palabras que tengan una longitud de 7 o más letras")
 x = r"[A-Za-záéíóú]{7,}"
@@ -14,19 +27,22 @@ resultax = re.findall(x,txt)
 for resultadosx in resultax:
     print(resultadosx)
 
+
 print("-----------------------------------------------------------------------")
 print("2.	Expresiones que NO finalicen con una vocal.")
-y = r"[A-Za-záéíóú]{1,}[^aeiou\s/W]\b"
+y = r"[A-Za-záéíóú]+[^aeiou\s/W]\b"
 resultay = re.findall(y,txt)
 for resultadosy in resultay:
     print(resultadosy) 
 
+
 print("-----------------------------------------------------------------------")
 print("3.	Las palabras que inicien con “M” donde la segunda letra no sea vocal")
-z = r"[M][^aeiouáéíóú]\w{1,}"
+z = r"[M][^aeiouáéíóú]\w+"
 resultaz = re.findall(z,txt)
 for resultadosz in resultaz:
     print(resultadosz)
+
 
 print("-----------------------------------------------------------------------")
 print("4.	Expresiones encerradas entre comillas")
@@ -35,12 +51,14 @@ resultaa = re.findall(a,txt)
 for resultadosa in resultaa:
     print(resultadosa)
 
+
 print("-----------------------------------------------------------------------")
 print("5.	Ip’s")
 b = r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 resultab = re.findall(b,txt)
 for resultadosb in resultab:
     print(resultadosb)
+
 
 print("-----------------------------------------------------------------------")
 print("6.	Horas")
@@ -49,12 +67,14 @@ resultac = re.findall(c,txt)
 for resultadosc in resultac:
     print(resultadosc)
 
+
 print("-----------------------------------------------------------------------")
 print("7.	Telefonos")
 d = r"[0-9]{1,3}\s[0-9]{1,3}\s[0-9]{1,4}"
 resultad = re.findall(d,txt)
 for resultadosd in resultad:
     print(resultadosd)
+
 
 print("-----------------------------------------------------------------------")
 print("8.	Correos electrónicos")
@@ -63,6 +83,7 @@ resultae = re.findall(e,txt)
 for resultadose in resultae:
     print(resultadose)
 
+
 print("-----------------------------------------------------------------------")
 print("9.	Url´s")
 f = r"https://+\w+[.]\w+"
@@ -70,9 +91,10 @@ resultaf = re.findall(f,txt)
 for resultadosf in resultaf:
     print(resultadosf)
 
+
 print("-----------------------------------------------------------------------")
-print("10.	Código postal")
-g = r"\d{5}"
+print("10.	Código postal de Quintana Roo")
+g = r"\b[7][7]\d{3}"
 resultag = re.findall(g,txt)
 for resultadosg in resultag:
     print(resultadosg)
